4b0bcad02ff74f87a03b31cc2ddbb5e31338557f
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-19 14:59:06 +0300

    Updated Less30
    ..

--- bin/Less30.rb
+++ bin/Less30.rb
@@ -20,3 +20,15 @@ def compute2(ary)
   return nil unless ary
   ary.map { |(a, b)| !b.nil? ? a + b : a }
 end
+
+first, *middle, last = [42, 43, 44, 45, 46, 47]
+
+def median(*list)
+  return nil if list.empty?
+  sorted = list.sort
+  if ((list.length%2)==1)
+    sorted[list.length/2]
+  else
+    (sorted[list.length/2-1]+sorted[list.length/2])/2.0
+  end
+end
