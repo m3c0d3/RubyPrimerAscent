0d6bb10ca7e5425a7fd940efa01a9b2b9cf92ad8
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-20 15:45:27 +0300

    Update Less32.rb

--- bin/Less32.rb
+++ bin/Less32.rb
@@ -1,3 +1,4 @@
+#Modify the above example to make the Stack class statically sized. push and pop should return nil if the stack is overflowing or underflowing respectively. Implement private predicate methods full? and empty? and public method size that returns the length of the stack, and look that returns the value on the top of the stack. 
 class Stack
   def initialize(size)
     @store = Array.new
