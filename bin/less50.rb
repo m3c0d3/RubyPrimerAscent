f2c0f17f9af05f42eab50da671af76e942f9dfa8
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-24 16:19:07 +0300

    Create less50.rb

---
+++ bin/less50.rb
@@ -0,0 +1,14 @@
+EXAMPLE_SECRETS = ["het", "keca", "si", nil, "iel"]
+def decode_all(ary)
+  begin
+    ary.each {|s| decode(s) }
+  rescue Exception => e
+    puts "Rescued" + e.to_s
+  end
+end
+
+def decode(jumble)
+  secret = jumble.split("").rotate(3).join("")
+  announce(secret)
+  secret
+end
