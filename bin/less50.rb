8f2a14bc9e25b85558b3a2a47164607c95e89b38
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-25 09:04:30 +0300

    Update less50.rb

--- bin/less50.rb
+++ bin/less50.rb
@@ -16,3 +16,17 @@ end
 def decode_all2(ary) 
   ary.each {|s| decode(s) } rescue "it's okay, little buddy." 
 end
+
+
+def string_slice(*ary)
+  raise ArgumentError, 'More than 5 passed in' if ary.size>5
+  result = ary.collect do |e| 
+    if e.length<3 
+      raise IndexError, 'string is less than 3 characters long'
+    else
+      e[0..2]
+    end
+  end
+  puts result
+  result  
+end
