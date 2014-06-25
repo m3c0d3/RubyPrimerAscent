bb3d97eb6ca06dceb33894d87feff4ec4c158b0f
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-25 15:29:15 +0300

    Create less61.rb

---
+++ bin/less61.rb
@@ -0,0 +1,11 @@
+def repetitive_array_of_strings
+  ["Wow,", "this", "is", "a", "pretty", "long", "list", "of", "words", "and", "it", "took", "me", "a", "long", "time", "to", "type", "because", "of", "all", "those", "darn", "quote", "characters.", "Geez."]
+end
+
+def array_of_words_literal
+  %w[With this double-u shorthand it wasn't very hard at all to type out this list of words. Heck, I was even able to use double-quotes like "these"!]
+end
+
+print repetitive_array_of_strings
+print "\n\n"
+print array_of_words_literal
