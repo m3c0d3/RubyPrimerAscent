cf50a988476b8d156572c9ba49202582db01f356
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-26 11:03:31 +0300

    Update less62.rb

--- bin/less62.rb
+++ bin/less62.rb
@@ -5,3 +5,17 @@ def awkward_sheep
   end
 end # create a class here with a method 'speak'
 end
+
+module Fence
+  # create your class here
+  Sheep = Class.new do
+    def speak
+      "Bah."
+    end
+  end
+end
+
+def call_sheep
+  # create a new sheep and make it talk!
+  Fence::Sheep.new.speak
+end
