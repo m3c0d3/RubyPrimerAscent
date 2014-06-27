84d4aa786302779568f1f09a490b8a403d2eb458
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-27 16:45:01 +0300

    Update less71.rb

--- bin/less71.rb
+++ bin/less71.rb
@@ -2,3 +2,10 @@ def hash_keys(hash)
   hash.map{ |e| e[0] }
   #hash.map { |pair| pair.first }
 end
+
+def occurrences(str)
+  str.downcase.scan(/\w+/).inject( Hash.new(0) ) do |acc, e| 
+    acc.update(e => (acc[e]+1) )
+    acc
+  end
+end
