19b0410c5ae392df8d2dec5f6cdb341bfdcbdb10
m3c0d3 <m3c0d3@users.noreply.github.com>
2014-06-30 12:27:39 +0300

    Create less93.rb

---
+++ bin/less93.rb
@@ -0,0 +1,35 @@
+class Object
+  def superclasses
+    # your code here    
+    result = []
+    parent = self.superclass  
+    while not parent.nil? do
+    	result<<parent
+    	parent = parent.superclass
+    end
+    result
+  end
+end
+
+class Bar
+end
+
+class Foo < Bar
+end
+
+p Foo.superclasses  # should be [Bar, Object, BasicObject]
+
+class Object
+  def superclasses_sol(klass = self.superclass)
+    return [] if klass.nil?
+    [klass] + superclasses(klass.superclass)
+  end
+end
+
+class Bar
+end
+
+class Foo < Bar
+end
+
+p Foo.superclasses  # should be [Bar, Object, BasicObject]
